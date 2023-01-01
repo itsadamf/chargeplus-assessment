4d3ffd4949671d9e613e25bb35b4736ae5ec7993
Adam Fikri <adamdecimo@gmail.com>
2023-01-01 17:06:46 +0800

    add constant file

---
+++ constant.py
@@ -0,0 +1,11 @@
+weekday = {
+    "first_3_hours": 3.00,
+    "sub_hour": 1.50,
+    "max_per_day": 20.00
+}
+
+weekend = {
+    "first_3_hours": 5.00,
+    "sub_hour": 2.00,
+    "max_per_day": 40.00
+}
